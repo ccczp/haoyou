0004a72a2b0081f22e0debee3d82a5815196842c
‘‘1093328275@qq.com <1093328275@qq.com>
2021-03-07 15:47:48 +0800

    project build

---
+++ src/main/java/com/czp/haoyou/HaoyouApplication.java
@@ -0,0 +1,13 @@
+package com.czp.haoyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaoyouApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaoyouApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/czp/haoyou/HaoyouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.czp.haoyou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaoyouApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
